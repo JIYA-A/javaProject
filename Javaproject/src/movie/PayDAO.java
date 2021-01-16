e37e5b3e6d8243a40e11e7ababcbdc6c581394d8
xoghd <xoghd@ITVINCENT>
2021-01-16 13:00:17 +0900

    PayDAO 추가

--- Javaproject/src/movie/PayDAO.java
+++ Javaproject/src/movie/PayDAO.java
@@ -6,7 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class SestDAO {
+public class PayDAO {
 	private Connection conn;
 	private PreparedStatement pst;
 	private ResultSet rs;
