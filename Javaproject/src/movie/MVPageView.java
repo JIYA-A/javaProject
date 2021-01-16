4b22a9069ecdde129c62bfa6c07497d7c9d813bc
Chosun <Chosun@DESKTOP-LFSHGNF>
2021-01-16 13:56:36 +0900

    PageView 추가

---
+++ Javaproject/src/movie/MVPageView.java
@@ -0,0 +1,106 @@
+package movie;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.SpringLayout;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JList;
+import javax.swing.JTable;
+import javax.swing.JButton;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class MVPageView {
+
+	private JFrame frame;
+	private JTable table;
+	
+	private DefaultTableModel model; //JTable 데이터 추가, 삭제
+	private int row; //선택한 행의 위치
+	
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MVPageView window = new MVPageView();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	
+	public MVPageView() {
+		initialize();
+	}
+
+	
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		SpringLayout springLayout = new SpringLayout();
+		frame.getContentPane().setLayout(springLayout);
+		
+		JPanel panel = new JPanel();
+		springLayout.putConstraint(SpringLayout.NORTH, panel, 0, SpringLayout.NORTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, panel, 0, SpringLayout.WEST, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, panel, 70, SpringLayout.NORTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, panel, 434, SpringLayout.WEST, frame.getContentPane());
+		frame.getContentPane().add(panel);
+		
+		JLabel lblNewLabel = new JLabel("My \uC608\uB9E4 \uB0B4\uC5ED");
+		springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel, 53, SpringLayout.SOUTH, panel);
+		springLayout.putConstraint(SpringLayout.WEST, lblNewLabel, 66, SpringLayout.WEST, frame.getContentPane());
+		frame.getContentPane().add(lblNewLabel);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		springLayout.putConstraint(SpringLayout.NORTH, scrollPane, 28, SpringLayout.SOUTH, lblNewLabel);
+		springLayout.putConstraint(SpringLayout.WEST, scrollPane, 48, SpringLayout.WEST, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, scrollPane, 240, SpringLayout.SOUTH, lblNewLabel);
+		springLayout.putConstraint(SpringLayout.EAST, scrollPane, -53, SpringLayout.EAST, frame.getContentPane());
+		frame.getContentPane().add(scrollPane);
+		
+		
+		//JTable 데이터초기화
+		//DAO,AO -> list에 넣어 배열로 구성 후 출력
+
+		
+		scrollPane.setViewportView(table);
+		
+		JPanel panel_1 = new JPanel();
+		springLayout.putConstraint(SpringLayout.NORTH, panel_1, 21, SpringLayout.SOUTH, scrollPane);
+		springLayout.putConstraint(SpringLayout.WEST, panel_1, 48, SpringLayout.WEST, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, panel_1, 56, SpringLayout.SOUTH, scrollPane);
+		springLayout.putConstraint(SpringLayout.EAST, panel_1, 381, SpringLayout.WEST, frame.getContentPane());
+		frame.getContentPane().add(panel_1);
+		SpringLayout sl_panel_1 = new SpringLayout();
+		panel_1.setLayout(sl_panel_1);
+		
+		JButton removeBtn = new JButton("\uCDE8\uC18C");
+		removeBtn.addActionListener(new ActionListener() {
+	      	public void actionPerformed(ActionEvent e) {
+	      	}
+	      });
+		removeBtn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				model.removeRow(row);
+	      		System.out.println("선택한 행이 삭제되었습니다.");
+			}
+		});
+		sl_panel_1.putConstraint(SpringLayout.SOUTH, removeBtn, -2, SpringLayout.SOUTH, panel_1);
+		sl_panel_1.putConstraint(SpringLayout.EAST, removeBtn, -10, SpringLayout.EAST, panel_1);
+		panel_1.add(removeBtn);
+	}
+}
