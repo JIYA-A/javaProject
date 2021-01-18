ec1bbdfd7b46a4b7504ba6c37c4db17739dc50e0
Chosun <Chosun@DESKTOP-LFSHGNF>
2021-01-18 10:14:26 +0900

    수정

--- Javaproject/src/movie/MVPayView.java
+++ Javaproject/src/movie/MVPayView.java
@@ -227,7 +227,10 @@ public class MVPayView {
 			public void mouseClicked(MouseEvent e) {
 				//클릭 시, 결제완료창을 띄우기
 				JOptionPane.showMessageDialog(null, "결제가 완료되었습니다.", "", JOptionPane.PLAIN_MESSAGE);
-				//DB저장
+				//DB저장하기
+				
+				
+				
 				
 				//예매하기 창으로 돌아가기
 				new MVRvView().main(null);
